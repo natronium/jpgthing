9dee5bfde79e8aea6bad6c0b1a32afadb35a38e4
na <25114018+Natronium@users.noreply.github.com>
2023-10-13 17:16:33 -0400

    Split into modules and publicize the JIF structs
    All the parser/struct things have been moved to jpeg, and the
    `parse_with` helpers to `helpers`. Additionally, all the jpeg structs
    and their fields have been made public, since that's going to be part of
    the point of this once it's done: a library for poking at jpeg-guts in
    addition to an application for doing the same.
    
    This change has the added benefit of getting rid of all the warnings
    about the many unused fields

---
+++ src/helpers.rs
@@ -0,0 +1,68 @@
+use std::{
+    io::{Read, Seek, SeekFrom},
+    iter::from_fn,
+};
+
+use binrw::{parser, BinRead, BinResult, Endian};
+
+//stolen and readapted from binrw::helpers::{until_eof, until_eof_with}
+//TODO: understand how this works and why i couldn't write a non-clojurey generic version
+pub(crate) fn until_invalid<Reader, T, Arg, Ret>(
+    reader: &mut Reader,
+    endian: Endian,
+    args: Arg,
+) -> BinResult<Ret>
+where
+    T: for<'a> BinRead<Args<'a> = Arg>,
+    Reader: Read + Seek,
+    Arg: Clone,
+    Ret: FromIterator<T>,
+{
+    until_invalid_with(T::read_options)(reader, endian, args)
+}
+
+pub(crate) fn until_invalid_with<Reader, T, Arg, ReadFn, Ret>(
+    read: ReadFn,
+) -> impl Fn(&mut Reader, Endian, Arg) -> BinResult<Ret>
+where
+    Reader: Read + Seek,
+    Arg: Clone,
+    ReadFn: Fn(&mut Reader, Endian, Arg) -> BinResult<T>,
+    Ret: FromIterator<T>,
+{
+    move |reader, endian, args| {
+        from_fn(|| match read(reader, endian, args.clone()) {
+            ok @ Ok(_) => Some(ok),
+            Err(_) => None,
+        })
+        .fuse()
+        .collect()
+    }
+}
+
+//TODO i'm pretty sure there's a more idiomatic way to do this
+//TODO i'm pretty sure there's also a faster way to do this
+#[parser(reader)]
+pub(crate) fn parse_ecs() -> BinResult<Vec<u8>> {
+    let mut ret = Vec::new();
+    let mut byte_buf = [0; 1];
+    loop {
+        reader.read_exact(&mut byte_buf)?;
+        let byte = byte_buf[0];
+        if byte != 0xff {
+            ret.push(byte);
+            continue;
+        } else if byte == 0xff {
+            reader.read_exact(&mut byte_buf)?;
+            let next_byte = byte_buf[0];
+            if next_byte == 0x00 {
+                ret.push(byte);
+            } else if next_byte != 0x00 {
+                reader.seek(SeekFrom::Current(-2))?;
+                break;
+            }
+        }
+    }
+
+    return Ok(ret);
+}
---
+++ src/jpeg.rs
@@ -0,0 +1,192 @@
+use crate::helpers::{parse_ecs, until_invalid};
+use binrw::binread;
+
+/*
+A (JIF) Jpeg File is:
+SOI, Frame, EOI
+
+A Frame is:
+[Tables/Misc], Frame header, Scan_1, [DNL], [Scan_2, ..., Scan_last]
+
+A Frame Header is:
+SOF_n, Lf:u16, P:u8, Y:u16, X:u16, Nf:u8, Component-Specification Parameters
+
+Component-Specification Parameters:
+C_1:u8, H_1:u4, V_1:u4, Tq_1:u8, ... Nf times
+
+A Scan is:
+[Tables/Misc], Scan Header [ECS_0, RST_0, ..., ECS_last-1, RST_last-1], ECS_last
+
+An Etropy-coded segment (ECS) is:
+<MCU_1>, <MCU_2>, ..., <MCU_Ri> // when there's a reset interval
+<MCU_n>, <MCU_n+1>, ..., <MCU_last> // for ECS_last
+*/
+
+/*
+JIF:
+0xff+ :marker-byte: [Marker contents]
+markers:
+    Start of Frame:
+
+*/
+
+
+
+#[binread]
+#[derive(Debug)]
+#[br(magic = 0xFFD8u16)]
+pub struct JIF {
+    pub jpeg_frame: Frame,
+    #[br(magic = 0xFFD9u16, dbg, temp)]
+    pub _eoi: (),
+}
+
+#[binread]
+#[derive(Debug)]
+pub struct Frame {
+    #[br(parse_with = until_invalid)]
+    pub misc_tables: Vec<MiscSegment>,
+    pub header: FrameHeader,
+    pub scan_1: Scan,
+    #[br(try)]
+    pub dnl: Option<DNL>,
+    #[br(parse_with = until_invalid)]
+    pub remaining_scans: Vec<Scan>,
+}
+
+#[binread]
+#[derive(Debug)]
+pub enum MiscSegment {
+    #[br(magic = 0xFFDBu16)]
+    QuantizationTable {
+        len: u16,
+        #[br(count = len - 2)]
+        body: Vec<u8>,
+    },
+    #[br(magic = 0xFFC4u16)]
+    HuffmanTable {
+        len: u16,
+        #[br(count = len - 2)]
+        body: Vec<u8>,
+    },
+    #[br(magic = 0xFFCCu16)]
+    ArithmeticConditioningTable {
+        len: u16,
+        #[br(count = len - 2)]
+        body: Vec<u8>,
+    },
+    #[br(magic = 0xFFDDu16)]
+    RestartIntervalDefinition {
+        len: u16,
+        #[br(count = len - 2)]
+        body: Vec<u8>,
+    },
+    #[br(magic = 0xFFFEu16)]
+    Comment {
+        len: u16,
+        #[br(count = len - 2)]
+        body: Vec<u8>,
+    },
+    //FFE0-FFEF
+    #[br(magic = 0xFFu8)]
+    #[br(assert( n >= 0xE0 && n <= 0xEF))]
+    AppData {
+        n: u8,
+        len: u16,
+        #[br(count = len - 2)]
+        body: Vec<u8>,
+    },
+}
+
+#[binread]
+#[derive(Debug)]
+#[br(magic = 0xFFDCu16)]
+pub struct DNL {
+    pub len: u16, //is always = 4. 2 bytes for this field, 2 for the next
+    pub nl: u16,
+}
+
+#[binread]
+#[derive(Debug)]
+#[br(assert(n >= 0xC0 && n <= 0xCF && n != 0xC4 && n != 0xC8 && n!= 0xCC))]
+#[br(assert(_marker == 0xFF))]
+pub struct SOFMarker {
+    pub _marker: u8,
+    pub n: u8,
+}
+
+#[binread]
+#[derive(Debug)]
+pub struct FrameHeader {
+    pub sof_marker: SOFMarker,
+    pub lf: u16,
+    pub p: u8,
+    pub y: u16,
+    pub x: u16,
+    pub nf: u8,
+    #[br(count = nf)]
+    pub component_parameters: Vec<FrameComponentParameterSet>,
+}
+
+#[binread]
+#[derive(Debug)]
+pub struct FrameComponentParameterSet {
+    pub c: u8,
+    #[br(temp)]
+    pub _raw_h_v: u8,
+    #[br(calc = (_raw_h_v & 0b1111_0000) >> 4)]
+    pub h: u8, //u4
+    #[br(calc = _raw_h_v & 0b0000_1111)]
+    pub v: u8, //u4
+    pub tq: u8,
+}
+
+#[binread]
+#[derive(Debug)]
+#[br(assert(_raw_n >= 0xD0 && _raw_n <= 0xD7))]
+#[br(assert(_marker == 0xFF))]
+pub struct RSTMarker {
+    pub _marker: u8,
+    pub _raw_n: u8,
+    #[br(calc = _raw_n & 0b0000_1111)]
+    pub n: u8,
+}
+
+#[binread]
+#[derive(Debug)]
+pub struct Scan {
+    #[br(parse_with = until_invalid)]
+    pub misc_tables: Vec<MiscSegment>,
+    pub header: ScanHeader,
+    #[br(parse_with = parse_ecs)]
+    pub raw_ecs_data: Vec<u8>,
+}
+
+#[binread]
+#[derive(Debug)]
+#[br(magic = 0xFFDAu16)]
+pub struct ScanHeader {
+    pub len: u16,
+    pub ns: u8,
+    #[br(count = ns)]
+    pub component_parameters: Vec<ScanComponentParameterSet>,
+    pub ss: u8,
+    pub se: u8,
+    pub _raw_ah_al: u8,
+    #[br(calc = (_raw_ah_al &0b1111_0000) >> 4)]
+    pub ah: u8, //u4
+    #[br(calc = (_raw_ah_al &0b0000_1111) >> 0)]
+    pub al: u8, //u4
+}
+
+#[binread]
+#[derive(Debug)]
+pub struct ScanComponentParameterSet {
+    pub cs: u8,
+    #[br(temp)]
+    pub _raw_td_ta: u8,
+    #[br(calc = (_raw_td_ta &0b1111_0000) >> 4)]
+    pub td: u8, //u4
+    #[br(calc = (_raw_td_ta &0b0000_1111) >> 0)]
+    pub ta: u8, //u4
+}
